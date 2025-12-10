d2dd15d491a23d454f8604a509362cb34bd4f968
Fenik <richard.kunz18@gmail.com>
2025-12-10 13:08:15 +0100

    TestPush

--- Mob/Pokemon.cs
+++ Mob/Pokemon.cs
@@ -8,6 +8,24 @@ namespace Mob
 {
     internal class Pokemon
     {
-        int attackPoints 
+        public string Name { get; }
+        public int AttackPower { get; private set; }
+        public int DefensePower { get; private set; }
+        public int HealthPoints { get; set; }
+        public string Weapon { get; private set; }
+        public bool InCover { get; private set; }
+
+        public Pokemon(string name, int attackPower, int defensePower, int healthPoints, string weapon)
+        {
+            Name = name;
+            HealthPoints = healthPoints;
+            Weapon = weapon;
+            AttackPower = attackPower * ;
+            DefensePower = defensePower
+            this.positionX = positionX;
+            this.positionY = positionY;
+            Console.WriteLine($"PMC {this.Name} created: ");
+        }
+
     }
 }
